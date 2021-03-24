f734d9af1b68736b90638ed0e935ce5c3f8a1edf
dami kim <iimadmik@gmail.com>
2021-03-24 22:56:59 +0900

    draft initial structure

---
+++ src/test/java/com/imadmik/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imadmik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
